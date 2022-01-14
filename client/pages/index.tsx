a800eea89959802c6048faf6208b34e43b31315f
이지호 <cctv1998@naver.com>
2022-01-14 15:21:17 +0900

    Feat : fetch api

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,8 +1,20 @@
-import React from "react";
-import Head from "next/head";
+import React, { useEffect, useState } from "react";
 import Seo from "components/Seo";
 
+const API_KEY = "5abee3cc86cd042648c65f9088106a23";
+
 const Home = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const data = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      console.log(data);
+    })();
+  }, []);
   return (
     <>
       <Seo title="Home" />
