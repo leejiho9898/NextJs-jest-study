94e90797087111c641c77f526b9ea1fda7a69bb8
이지호 <cctv1998@naver.com>
2022-01-16 23:07:33 +0900

    Feat : 환경 설정

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/movies",
+  //       destination: `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`,
+  //     },
+  //   ];
+  // },
+};
